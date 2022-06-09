aba0cf7cdcee053f54f379759b8b33ca97b6faec
Sukriti Thapa <thapa.sukriti@gmail.com>
2022-06-08 20:36:13 -0400

    add random quote generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios"; //to make get request
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+  //most import lifecylce method - executes at render of our component
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  //method: functoin that belongs to a class
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice: advice });
+
+        //console.log(advice); //destructure response
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
